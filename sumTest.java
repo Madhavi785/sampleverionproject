590021b9129f3a208d1f118c7ca51ded91b4fb86
Madhavi785 <madhavibalijepalli07@gmail.com>
2024-08-29 11:06:09 +0530

    Add files via upload

---
+++ sumTest.java
@@ -0,0 +1,22 @@
+package com.verizon;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+class CalculatorTest2 {
+	Calculator c;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("testing class started");
+	}
+	
+	
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
